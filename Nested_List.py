2822a9da185696f1db5ef5ff23479951ad2c27bf
Garima Choudhary <80445708+22GC22@users.noreply.github.com>
2021-07-01 17:02:55 +0530

    Create Nested_List.py

---
+++ Nested_List.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    arr = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        arr.append([name,score])
+second_min = sorted(list(set([marks for name, marks in arr])))[1]
+print('\n'.join([a for a,b in sorted(arr) if b == second_min]))
